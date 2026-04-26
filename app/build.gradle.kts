d38ef65d437f0e34309835ec23105614d56bf43d
AndyShaman <AndyShaman@users.noreply.github.com>
2026-04-26 13:10:39 +0300

    Revert "chore(smoke): TEMP auto-trigger AdbOnDeviceClient.connect on startup"
    This reverts commit 6f230e042c0cc83480f6bd89a22b3299acc0eb05.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         // on DiLink Android 12 (requestLegacyExternalStorage works).
         // targetSdk 30+ would break listFiles() on /storage/emulated/0/energydata/
         targetSdk = 29
-        versionCode = 253
-        versionName = "2.4.13-c0smoke"
+        versionCode = 252
+        versionName = "2.4.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
